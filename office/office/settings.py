2a9fda3531297a65fa59528c089cf60bfd1da4cb
Bajajyatharth <yatharthbajaj1508@gmail.com>
2025-07-16 20:44:49 +0530

    static added

--- office/office/settings.py
+++ office/office/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-52xs_!9hbbfthfniq0i!4+sm6h&(n=t@4o3%84xt=i+_+%xyns
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bajajyatharth.pythonanywhere.com']
 
 
 # Application definition
@@ -113,12 +113,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
